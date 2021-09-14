d3bac3612319eb4f3d37f4574a6d6cc129c049f0
DejanKatovic <88758044+DejanKatovic@users.noreply.github.com>
2021-09-14 13:18:20 -0400

    #24 Updated

--- client/src/components/ProfilePhoto/ProfilePhoto.tsx
+++ client/src/components/ProfilePhoto/ProfilePhoto.tsx
@@ -11,21 +11,10 @@ import ProfileAvatar from '../ProfileAvatar/ProfileAvatar';
 
 interface Props {
   loggedInUser: User;
-  handleDrawerToggle?: () => void;
 }
 
 const ProfilePhoto = ({ loggedInUser }: Props): JSX.Element => {
-  const [search, setSearch] = useState<string>('test');
-  const [newChatUser, setNewChatUser] = useState<User | null>(null);
   const classes = useStyles();
-
-  // React.FormEvent<FormControl & FormControlProps>)
-  const handleChange = (e: ChangeEvent<HTMLInputElement>, newInputValue: string) => {
-    setSearch(newInputValue);
-    if (newChatUser) {
-      setNewChatUser(null);
-    }
-  };
 
   return (
     <Paper elevation={5} className={classes.root}>
