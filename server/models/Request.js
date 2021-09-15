ee21ef18fe9d17506d4f01331445375e66c5e50c
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-09-15 21:04:00 +0100

    Changed the ref attribute of the user field to correctly refer to the user document

--- server/models/Request.js
+++ server/models/Request.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const requestSchema = new mongoose.Schema({
   user: { 
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "user",
     required: true
   },
   sitterId: {
