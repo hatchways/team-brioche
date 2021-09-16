117830d34e4cad8efa45ecee966c9388e18bc3fc
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-09-16 02:54:33 +0100

    bugFix: updated the reference to the User model in the Request schema

--- server/models/Request.js
+++ server/models/Request.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const requestSchema = new mongoose.Schema({
   user: { 
     type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    ref: "User",
     required: true
   },
   sitterId: {
