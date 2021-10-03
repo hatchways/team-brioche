cffe08145871971c83a7f201db09bf2d328c2eac
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-10-03 21:28:49 +0100

    Updated the App.tsx file after merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,11 @@ import AdapterMoment from '@mui/lab/AdapterMoment';
 import { LocalizationProvider } from '@mui/lab';
 import { theme } from './themes/theme';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import { AuthProvider } from './context/useAuthContext';
-import { SocketProvider } from './context/useSocketContext';
-import { SnackBarProvider } from './context/useSnackbarContext';
 import Login from './pages/Accounts/Login/Login';
 import Signup from './pages/Accounts/Signup/SignUp';
 import EditProfile from './pages/EditProfile/EditProfile';
 import Booking from './pages/Booking/Booking';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Profile from './pages/Profile/Profile';
 import HomePage from './pages/HomePage/HomePage';
 import ProfileDetails from './pages/ProfileDetails/ProfileDetails';
 import { AuthProvider } from './context/useAuthContext';
@@ -28,24 +24,24 @@ function App(): JSX.Element {
       <CssBaseline />
       <BrowserRouter>
         <SnackBarProvider>
-          <AuthProvider>
-            <SocketProvider>
-              <LocalizationProvider dateAdapter={AdapterMoment}>
-                <Layout>
-                  <Switch>
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/signup" component={Signup} />
-                    <Route exact path="/home" component={HomePage} />
-                    <Route exact path="/booking" component={Booking} />
-                    <Route path="/profile-listings" component={ProfileListings} />
-                    <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-                    <ProtectedRoute exact path="/edit-profilel" component={EditProfile} />
-                    <Route path="*" render={() => <Redirect to="/login" />} />
-                  </Switch>
-                </Layout>
-              </LocalizationProvider>
-            </SocketProvider>
-          </AuthProvider>
+          {/* <AuthProvider> */}
+          <SocketProvider>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+              <Layout>
+                <Switch>
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/signup" component={Signup} />
+                  <Route exact path="/home" component={HomePage} />
+                  <Route exact path="/booking" component={Booking} />
+                  <Route path="/profile-listings" component={ProfileListings} />
+                  <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+                  <ProtectedRoute exact path="/edit-profilel" component={EditProfile} />
+                  <Route path="*" render={() => <Redirect to="/login" />} />
+                </Switch>
+              </Layout>
+            </LocalizationProvider>
+          </SocketProvider>
+          {/* </AuthProvider> */}
         </SnackBarProvider>
       </BrowserRouter>
     </MuiThemeProvider>
