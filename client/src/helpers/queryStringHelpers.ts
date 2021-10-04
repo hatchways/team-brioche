b273487a1b959f41d933cfedee77b6d9ec8acd5b
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-10-04 05:23:49 +0100

    Update queryStringHelpers.ts
    - removed unnecessary string check

--- client/src/helpers/queryStringHelpers.ts
+++ client/src/helpers/queryStringHelpers.ts
@@ -12,15 +12,15 @@ const recoverWhiteSpace = (str: string): string => str.split('-').join(' ');
 
 export const verfyProfileQuery = (queryString: ParsedQuery<string>): Query => ({
   address: {
-    test: queryString && queryString.address && queryString.address.length < maxLengthPermitted ? true : false,
+    test: queryString.address && queryString.address.length < maxLengthPermitted ? true : false,
     value: queryString.address ? recoverWhiteSpace(queryString.address as string) : '',
   },
   dropInDate: {
-    test: queryString && queryString.dropInDate && isValidDateString(queryString.dropInDate as string) ? true : false,
+    test: queryString.dropInDate && isValidDateString(queryString.dropInDate as string) ? true : false,
     value: new Date(queryString.dropInDate as string).toString(),
   },
   dropOffDate: {
-    test: queryString && queryString.dropOffDate && isValidDateString(queryString.dropOffDate as string) ? true : false,
+    test: queryString.dropOffDate && isValidDateString(queryString.dropOffDate as string) ? true : false,
     value: new Date(queryString.dropOffDate as string).toString(),
   },
 });
