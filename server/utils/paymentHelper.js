74324f8d18b236ad27a08588de74af26e6ce6c2e
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-10-07 05:49:13 +0100

    Update paymentHelper.js

--- server/utils/paymentHelper.js
+++ server/utils/paymentHelper.js
@@ -48,6 +48,7 @@ async function getPaymentMethods(profile) {
       email: billingDetails.email,
       expMonth: card.exp_month,
       expYear: card.exp_year,
+      name: billingDetails.name,
     };
   });
 }
