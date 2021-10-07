c93bd694ed08d2b20bc497dde9568c43bca62024
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-10-07 06:08:54 +0100

    Squashed commit of the following:
    commit 74324f8d18b236ad27a08588de74af26e6ce6c2e
    Author: Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
    Date:   Thu Oct 7 05:49:13 2021 +0100
    
        Update paymentHelper.js
    
    commit 498583df40677ccbcf4fec83f98ad354d9f3bdce
    Author: Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
    Date:   Thu Oct 7 05:48:15 2021 +0100
    
        Update paymentHelper.js
    
    commit 7894fcfea8df5b5abf01fffe7cfcefbbbf51d4bf
    Author: Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
    Date:   Thu Oct 7 05:43:53 2021 +0100
    
        Minor updates
    
        - removed code duplications and comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,26 +26,26 @@ function App(): JSX.Element {
       <CssBaseline />
       <BrowserRouter>
         <SnackBarProvider>
-          {/* <AuthProvider> */}
-          <SocketProvider>
-            <LocalizationProvider dateAdapter={AdapterMoment}>
-              <Layout>
-                <Switch>
-                  <Route exact path="/login" component={Login} />
-                  <Route exact path="/signup" component={Signup} />
-                  <Route path="/profile-settings" component={ProfileSkeleton} />
-                  <Route exact path="/booking" component={Booking} />
-                  <Route exact path="/profile" component={Profile} />
-                  <Route exact path="/profile-listings" component={ProfileListings} />
-                  <ProtectedRoute exact path="/profiles/:id" component={ProfileDetails} />
-                  <ProtectedRoute exact path="/dashboard" component={Dashboard} />
-                  <ProtectedRoute exact path="/edit-profile" component={EditProfile} />
-                  <Route path="*" render={() => <Redirect to="/login" />} />
-                </Switch>
-              </Layout>
-            </LocalizationProvider>
-          </SocketProvider>
-          {/* </AuthProvider> */}
+          <AuthProvider>
+            <SocketProvider>
+              <LocalizationProvider dateAdapter={AdapterMoment}>
+                <Layout>
+                  <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+                    <Route path="/profile-settings" component={ProfileSkeleton} />
+                    <Route exact path="/booking" component={Booking} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route exact path="/profile-listings" component={ProfileListings} />
+                    <ProtectedRoute exact path="/profiles/:id" component={ProfileDetails} />
+                    <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+                    <ProtectedRoute exact path="/edit-profile" component={EditProfile} />
+                    <Route path="*" render={() => <Redirect to="/login" />} />
+                  </Switch>
+                </Layout>
+              </LocalizationProvider>
+            </SocketProvider>
+          </AuthProvider>
         </SnackBarProvider>
       </BrowserRouter>
     </MuiThemeProvider>
