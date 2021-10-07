3e1a165e3a053e5798e0876f33207c26973c901d
Ikechukwu Okerenwogba <ike.okerenwogba@gmail.com>
2021-10-07 19:45:34 +0100

    Update useStyles.ts

--- client/src/components/ProfilePayment/useStyles.ts
+++ client/src/components/ProfilePayment/useStyles.ts
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   container: {
     width: '100%',
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
   },
   heading: {
     fontWeight: theme.typography.fontWeightBold,
