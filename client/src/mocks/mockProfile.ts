f6bd582406eff4463985e37810871bb9a61b998e
Mayank Suchade <mayank.suchde.4500@gmail.com>
2021-10-08 09:08:25 -0400

    fixed minor bug and npm i

--- client/src/mocks/mockProfile.ts
+++ client/src/mocks/mockProfile.ts
@@ -70,7 +70,6 @@ const mockProfile: Profile = {
     ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum.',
   _id: '',
   dob: '',
-  user: mockLoggedInUser,
   profilePic: '/pics/profilepic-sample.jpg',
   coverPic: '/pics/cover-sample.jpg',
   galleryPics: ['/pics/dog1.jpg', '/pics/dog2.jpg', '/pics/dog3.jpg'],
